7d64e26457ee8e9a9cb44839f871b81e324d4e17
shaharcc <100072700+shaharcc@users.noreply.github.com>
2023-04-05 15:12:53 +0300

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -10,24 +10,20 @@
 #include "Exceptions.h"
 #include "Character.h"
 
-using namespace mtm;
+using namespace game_ns;
 
 const string empty_cell = " ";
 
-
-Game::Game(int height, int width): height(height), width(width)
-{
+Game::Game(int height, int width): height(height), width(width)  {
     if(height < 0 || width < 0){
         throw IllegalArgument();
     }
     this->board = makeBoard(height, width);
 }
 
-Game::Game(const Game& other): height(other.height), width(other.width)
-{
+Game::Game(const Game& other): height(other.height), width(other.width) {
     std::vector<std::shared_ptr<Character>>::const_iterator iterator = other.board.begin();
-    while(iterator != other.board.end())
-    {
+    while(iterator != other.board.end()) {
         if(*iterator == nullptr)
             board.push_back(nullptr);
         std::shared_ptr<Character> character((*iterator)->clone());
@@ -36,23 +32,16 @@ Game::Game(const Game& other): height(other.height), width(other.width)
     }
 }
 
-Game &Game::operator=(const Game& other) 
-{
+Game &Game::operator=(const Game& other) {
     if(this == &other)
         return *this;
     this->width = other.width;
     this->height = other.height;
 
-    /*NOT FINISHED:
-    NEED TO COPY THE BOARD
-    by insert? push_back? else?
-    */
-
     return *this;
 }
 
-void Game::addCharacter(const GridPoint& coordinates, std::shared_ptr<Character> character)
-{
+void Game::addCharacter(const GridPoint& coordinates, std::shared_ptr<Character> character) {
     if(!checkIfCellInBoard(coordinates))
         throw IllegalCell();
     int position = findPosition(coordinates);
@@ -65,12 +54,11 @@ void Game::addCharacter(const GridPoint& coordinates, std::shared_ptr<Character>
 }
 
 static std::shared_ptr<Character> makeCharacter(CharacterType type, Team team,
-                units_t health, units_t ammo, units_t range, units_t power)
-{
+                units_t health, units_t ammo, units_t range, units_t power) {
     if(health < 1 || ammo < 0 || range < 0 || power < 0)
         throw IllegalArgument();
 
-    switch (type){
+    switch (type) {
         case SOLDIER:
             return std::shared_ptr<Character>(new Soldier(health, ammo, range, power, team));
         case SNIPER:
@@ -80,8 +68,7 @@ static std::shared_ptr<Character> makeCharacter(CharacterType type, Team team,
     }
 }
 
-void Game::move(const GridPoint & src_coordinates, const GridPoint & dst_coordinates)
-{
+void Game::move(const GridPoint & src_coordinates, const GridPoint & dst_coordinates) {
     if(!checkIfCellInBoard(src_coordinates) || !checkIfCellInBoard(dst_coordinates))
         throw IllegalCell();
     int src_position = findPosition(src_coordinates);
@@ -105,8 +92,7 @@ void Game::move(const GridPoint & src_coordinates, const GridPoint & dst_coordin
     std::swap(src_iterator, dst_iterator);
 }
 
-void Game::attack(const GridPoint & src_coordinates, const GridPoint & dst_coordinates)
-{
+void Game::attack(const GridPoint & src_coordinates, const GridPoint & dst_coordinates) {
     if(!checkIfCellInBoard(src_coordinates) || !checkIfCellInBoard(dst_coordinates))
         throw IllegalCell();
     int src_position = findPosition(src_coordinates);
@@ -122,9 +108,7 @@ void Game::attack(const GridPoint & src_coordinates, const GridPoint & dst_coord
     }
 }
 
-
-void Game::attack(const GridPoint & src_coordinates, const GridPoint & dst_coordinates)
-{
+void Game::attack(const GridPoint & src_coordinates, const GridPoint & dst_coordinates) {
     if(!checkIfCellInBoard(src_coordinates) || !checkIfCellInBoard(dst_coordinates))
         throw IllegalCell();
     int attacker_position = findPosition(src_coordinates);
@@ -140,18 +124,16 @@ void Game::attack(const GridPoint & src_coordinates, const GridPoint & dst_coord
     std::shared_ptr<Character> attacked = board.at(attacked_postion);
     curr_character->makeAttack(board, src_coordinates, dst_coordinates, 
                             attacker_position, attacked_postion, attacked);
-    //check if out of game for all the characters involved
+    // check if out of game for all the characters involved
 
-    /* makeAttack needs to check for the exceptions:
+    /* makeAttack checks for the exceptions:
         throw OutOfRange();
         throw OutOfAmmo();
         throw IllegalTarget();
     */
 }
 
-
-void Game::reload(const GridPoint &coordinates)
-{
+void Game::reload(const GridPoint &coordinates) {
     int position = findPosition(coordinates);
     if(!checkIfCellInBoard(coordinates)){
         throw IllegalCell();
@@ -163,9 +145,8 @@ void Game::reload(const GridPoint &coordinates)
     curr_character->reloadAmmo();
 }
 
-namespace mtm{
-    ostream& operator<<(ostream& os, const Game& game)
-    {
+namespace game_ns {
+    ostream& operator<<(ostream& os, const Game& game) {
         std::string symbol;
 
         for(const std::shared_ptr<Character> &iterator : game.board){
@@ -177,12 +158,10 @@ namespace mtm{
         std::shared_ptr<Character> iterator = *game.board.end();
         string end = iterator->getSymbol(); //or '/0'
         return printGameBoard(os, symbol.c_str(), end.c_str(), game.width);
-        
     }
 }
 
-bool Game::isOver(Team* winningTeam=NULL) const
-{
+bool Game::isOver(Team* winningTeam=NULL) const {
     int i = 0;
     while(board.at(i) == nullptr)
         i++; //first appearance of a player
@@ -204,39 +183,33 @@ bool Game::isOver(Team* winningTeam=NULL) const
     }
 }
 
-std::vector<std::shared_ptr<Character>> Game::makeBoard(int height, int width)
-{
+std::vector<std::shared_ptr<Character>> Game::makeBoard(int height, int width) {
     std::vector<std::shared_ptr<Character>> board;
     for(int i = 0; i < height * width; i++)
         board.push_back(nullptr);
     return board;
 }
 
-int Game::findPosition(const GridPoint &coordinates)
-{
+int Game::findPosition(const GridPoint &coordinates) {
     int position = this->width * coordinates.row + coordinates.col;
     return position;
 }
 
-bool Game::checkIfCellInBoard(const GridPoint & coordinates)
-{
+bool Game::checkIfCellInBoard(const GridPoint & coordinates) {
     if((coordinates.row < 0 || coordinates.row > this->height - 1) ||
        (coordinates.col < 0 || coordinates.col > this->width - 1))
        return false;
     return true;
 }
 
-bool Game::checkIfCellIsEmpty(int position)
-{
+bool Game::checkIfCellIsEmpty(int position) {
     if(this->board[position] == nullptr)
         return true;
     return false;
 }
 
-int Game::getTotalSteps(const GridPoint & src_coordinates, const GridPoint & dst_coordinates)
-{
+int Game::getTotalSteps(const GridPoint & src_coordinates, const GridPoint & dst_coordinates) {
     int total_steps = abs(dst_coordinates.row - src_coordinates.row) + 
                       abs(dst_coordinates.col - src_coordinates.col);
     return total_steps;
 }
-
